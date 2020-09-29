dd1e8e974827aefce6fd61bb03451068561cfed5
Rômullo Cordeiro Rodrigues <romullocordeiro@gmail.com>
2020-09-29 03:08:42 -0300

    Updates

---
+++ precache-manifest.c303095f56d6cd9bb94db77108f2fa53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29929749f31ac20e259b9e0a8c59127",
+    "url": "/bikini-shop-landing-page/index.html"
+  },
+  {
+    "revision": "7138d803e4478902abc3",
+    "url": "/bikini-shop-landing-page/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1f4d5200ba71885adb6c",
+    "url": "/bikini-shop-landing-page/static/css/main.d776752e.chunk.css"
+  },
+  {
+    "revision": "7138d803e4478902abc3",
+    "url": "/bikini-shop-landing-page/static/js/2.69577c6f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bikini-shop-landing-page/static/js/2.69577c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f4d5200ba71885adb6c",
+    "url": "/bikini-shop-landing-page/static/js/main.71abfde3.chunk.js"
+  },
+  {
+    "revision": "bb73d05f53791831ca33",
+    "url": "/bikini-shop-landing-page/static/js/runtime-main.a0baa0c4.js"
+  }
+]);
